d6939b047c671818b3a809d4e3d33c3812a8876c
Jakub Zegzulka <jakub@zegzulka.cz>
2024-11-24 17:03:47 -0800

    Update camera.js

--- camera.js
+++ camera.js
@@ -14,7 +14,7 @@ function initializeCamera(containerId) {
   // Add placeholder image
   const placeholderImg = document.createElement("img");
   placeholderImg.src =
-    "https://framerusercontent.com/images/HNm7thqoI4ZL7nVploayqybotXg.png";
+    "https://framerusercontent.com/images/OJOmP4R6GPP4plCdnFFP6lIyrH8.png";
   placeholderImg.style.cssText = `
   width: 100%;
   height: 100%;
